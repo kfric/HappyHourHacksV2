8274fb1c0c1b1b14c8a2173522d19442faa8c02e
Karl Frick <karl.frick5@gmail.com>
2021-06-18 13:48:26 -0400

    All pages created

---
+++ ClientApp/src/pages/SignIn.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import 'bulma/css/bulma.min.css'
+
+export function SignIn() {
+  return (
+    <>
+      <h1 class="title is-1">Title 1</h1>
+    </>
+  )
+}
+
+export default SignIn
